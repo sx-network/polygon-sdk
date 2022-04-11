333d001d6de879c3c1f1b9eedd943ddd3c2d5eb0
Dan Kostiuk <daniel@nextgenbt.com>
2022-04-11 11:45:32 -0400

    Revert "log snapshot validators"
    This reverts commit 39dd101c576069942d9e7c4360df3bf4fc13fbd5.

--- consensus/ibft/ibft.go
+++ consensus/ibft/ibft.go
@@ -448,12 +448,7 @@ func (i *Ibft) isValidSnapshot() bool {
 	snap, err := i.getSnapshot(header.Number)
 
 	if err != nil {
-		i.logger.Debug("dgk - Error in getting snapshot for current node", "err", err)
 		return false
-	}
-
-	for index, validatorAddress := range snap.Set {
-		i.logger.Debug("dgk - current snapshot validators", "index", index, "validatorAddress", validatorAddress)
 	}
 
 	if snap.Set.Includes(i.validatorKeyAddr) {
@@ -465,7 +460,6 @@ func (i *Ibft) isValidSnapshot() bool {
 		return true
 	}
 
-	i.logger.Debug("dgk - Snapshot does not include current node")
 	return false
 }
 
