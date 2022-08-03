415a511554402d64f4ce41e9f484a2e81a4da8ac
Dan Kostiuk <daniel@nextgenbt.com>
2022-08-03 13:22:34 -0400

    add blockRewards to extraData pt 2

--- consensus/ibft/consensus_backend.go
+++ consensus/ibft/consensus_backend.go
@@ -145,8 +145,8 @@ func (i *backendIBFT) buildBlock(snap *Snapshot, parent *types.Header) (*types.B
 
 	header.Timestamp = uint64(headerTime.Unix())
 
-	// we need to include in the extra field the current set of validators
-	putIbftExtraValidators(header, snap.Set)
+	// we need to include in the extra field the current set of validators and block reward
+	putIbftExtraSnapshotData(header, snap.Set, snap.BlockReward)
 
 	transition, err := i.executor.BeginTxn(parent.StateRoot, header, i.validatorKeyAddr)
 	if err != nil {
