766f8a6d7dbca1c5760f1ac6df754e4b23a61ce7
Dan Kostiuk <daniel@nextgenbt.com>
2023-05-24 10:19:56 -0400

    try to fix getStorageAt issue

--- jsonrpc/eth_endpoint.go
+++ jsonrpc/eth_endpoint.go
@@ -6,7 +6,6 @@ import (
 	"math/big"
 
 	"github.com/hashicorp/go-hclog"
-	"github.com/umbracle/fastrlp"
 
 	"github.com/0xPolygon/polygon-edge/chain"
 	"github.com/0xPolygon/polygon-edge/helper/common"
@@ -361,23 +360,7 @@ func (e *Eth) GetStorageAt(
 		return nil, err
 	}
 
-	// TODO: GetStorage should return the values already parsed
-
-	// Parse the RLP value
-	p := &fastrlp.Parser{}
-
-	v, err := p.Parse(result)
-	if err != nil {
-		return argBytesPtr(types.ZeroHash[:]), nil
-	}
-
-	data, err := v.Bytes()
-	if err != nil {
-		return argBytesPtr(types.ZeroHash[:]), nil
-	}
-
-	// Pad to return 32 bytes data
-	return argBytesPtr(types.BytesToHash(data).Bytes()), nil
+	return argBytesPtr(result), nil
 }
 
 // GasPrice returns the average gas price based on the last x blocks
