1634db46eb8b55336bb87be1ed57c48a13b5c4bd
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2024-02-08 17:51:09 -0300

    12

--- datafeed/eventlistener.go
+++ datafeed/eventlistener.go
@@ -122,9 +122,10 @@ func (e EventListener) startListeningLoop() {
 
 			// @here
 			// e.datafeedService.config.OutcomeVotingPeriodSeconds = 30
-			res := e.datafeedService.sendCall("_votingPeriod");
-			e.logger.Debug("------------ RES", res);
-			
+			// res := e.datafeedService.sendCall("_votingPeriod");
+			e.datafeedService.sendCall();
+			// e.logger.Debug("------------ RES", res);
+
 			e.datafeedService.queueReportingTx(VoteOutcome, marketHashStr, -1)
 			e.datafeedService.storeProcessor.store.add(marketHashStr, uint64(blockTimestamp.Int64()))
 		case vLog := <-outcomeReportedLogs:
